242118c8b44c9d2f8639bb938e558710baf996fe
Mathias Klippinge <mathias.klippinge@gmail.com>
2017-12-01 09:14:13 +0100

    Draft collector

--- lib/phobos_prometheus/middleware/collector.rb
+++ lib/phobos_prometheus/middleware/collector.rb
@@ -10,12 +10,36 @@ module PhobosPrometheus
         @app = app
         @registry = options[:registry] || Prometheus::Client.registry
         @metrics_prefix = options[:metrics_prefix] || 'phobos_client'
-        # @counter_lb = options[:counter_label_builder] || COUNTER_LB
-        # @duration_lb = options[:duration_label_builder] || DURATION_LB
 
-        puts "===== PhobosPrometheus::Middleware::Collector, app = #{app}"
-        Phobos::Instrumentation.subscribe('listener.start') do |event|
-          puts "===== PhobosPrometheus::Middleware::Collector, event.payload = #{event.payload}"
+        init_event_metrics
+        subscribe_event_metrics
+      end
+
+      protected
+
+      EVENT_LABEL_BUILDER = proc do |event|
+        {
+          topic:    event.payload.topic,
+          group_id: event.payload.group_id,
+          handler:  event.payload.handler
+        }
+      end
+
+      def init_event_metrics
+        @event_total = @registry.counter(
+          :"#{@metrics_prefix}_total_events_handled",
+          'The total number of kafka events handled by the Phobos application.',
+        )
+        @event_durations = @registry.histogram(
+          :"#{@metrics_prefix}_events_handled_duration",
+          'The event consume duration in ms of the Phobos application.',
+        )
+      end
+
+      def subscribe_event_metrics
+        Phobos::Instrumentation.subscribe('listener.process_message') do |event|
+          @event_total.increment(EVENT_LABEL_BUILDER.call(event))
+          @event_durations.observe(EVENT_LABEL_BUILDER.call(event), event.duration)
         end
       end
     end
