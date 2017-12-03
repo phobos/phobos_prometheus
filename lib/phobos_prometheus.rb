40d5d8f724487e81939021f516d3f1f4ac788d56
Mathias Klippinge <mathias.klippinge@gmail.com>
2017-12-03 14:53:29 +0100

    Add batch metrics

--- lib/phobos_prometheus.rb
+++ lib/phobos_prometheus.rb
@@ -11,15 +11,21 @@ require 'phobos_prometheus/exporter'
 # Prometheus collector for Phobos
 module PhobosPrometheus
   class << self
-    attr_reader :message_collector, :config
+    attr_reader :message_collector, :batch_collector, :config
 
     def subscribe
       @message_collector ||= Collector.create('listener.process_message')
+      @batch_collector ||= Collector.create('listener.process_batch')
+
+      Phobos.logger.info { Hash(message: 'PhobosPrometheus subscribed', env: ENV['RACK_ENV']) }
+
       self
     end
 
     def configure(configuration)
       @config = Phobos::DeepStruct.new(fetch_settings(configuration))
+
+      Phobos.logger.info { Hash(message: 'PhobosPrometheus configured', env: ENV['RACK_ENV']) }
     end
 
     private
