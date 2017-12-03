834f53e96493018d9b0fe472f4c6c205fa44ad7d
Mathias Klippinge <mathias.klippinge@gmail.com>
2017-12-03 13:24:05 +0100

    Add specs for configure and rename register_subscriber to subscribe

--- spec/lib/phobos_prometheus_spec.rb
+++ spec/lib/phobos_prometheus_spec.rb
@@ -1,20 +1,46 @@
 RSpec.describe PhobosPrometheus do
+  let(:config_path) { 'spec/fixtures/phobos_prometheus.yml' }
+
   it 'has a version number' do
     expect(PhobosPrometheus::VERSION).not_to be nil
   end
 
-  describe '.register_subscriber' do
-    let(:options) { Hash(foo: :bar) }
-
-    it 'creates a collector object with passed options' do
-      expect(PhobosPrometheus::Collector).to receive(:new).with(options)
-      PhobosPrometheus.register_subscriber(options)
+  describe '.subscribe', :configured do
+    it 'creates a collector object' do
+      expect(PhobosPrometheus::Collector).to receive(:create)
+      PhobosPrometheus.subscribe
     end
 
     it 'memorizes the collector object' do
-      collector = PhobosPrometheus.register_subscriber(options).collector
-      collector2 = PhobosPrometheus.register_subscriber(options).collector
+      collector = PhobosPrometheus.subscribe.message_collector
+      collector2 = PhobosPrometheus.subscribe.message_collector
       expect(collector).to eql(collector2)
+    end
+  end
+
+  describe '.configure' do
+    it 'creates the configuration obj' do
+      PhobosPrometheus.configure(config_path)
+      expect(PhobosPrometheus.config).to_not be_nil
+      expect(PhobosPrometheus.config.metrics_prefix).to eq 'phobos_app'
+    end
+
+    context 'when using erb syntax in configuration file' do
+      it 'parses it correctly' do
+        PhobosPrometheus.configure('spec/fixtures/phobos_prometheus.yml.erb')
+
+        expect(PhobosPrometheus.config).to_not be_nil
+        expect(PhobosPrometheus.config.metrics_prefix).to eq('InjectedThroughERB')
+      end
+    end
+
+    context 'when providing hash with configuration settings' do
+      it 'parses it correctly' do
+        PhobosPrometheus.configure(metrics_prefix: 'foo')
+
+        expect(PhobosPrometheus.config).to_not be_nil
+        expect(PhobosPrometheus.config.metrics_prefix).to eq('foo')
+      end
     end
   end
 end
